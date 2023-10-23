41761ce43cad465bd16e9fd10fe98420739a153d
radityo.hanif <radityo.hanif@gmail.com>
2023-10-23 17:56:50 +0700

    kontribusi page

--- frontend/src/pages/public/kontribusi.tsx
+++ frontend/src/pages/public/kontribusi.tsx
@@ -1,15 +1,26 @@
-import { Text, Heading } from "@chakra-ui/react";
+import { Button, Heading, Text, Icon, Stack } from "@chakra-ui/react";
+import { FiGithub } from "react-icons/fi";
+
 import MobileLayout from "../../layout/mobile-layout";
+import { Link } from "react-router-dom";
 
 export default function Kontribusi() {
   return (
     <MobileLayout>
-      <Heading>
-        Kontribusi
-      </Heading>
-      <Text>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellat libero deserunt ea suscipit quidem? Quaerat omnis pariatur harum unde aspernatur et ab vitae fugiat alias illo eveniet, culpa aperiam possimus?
-      </Text>
+      <Stack justifyContent={'center'} alignItems={'center'} height={'100%'}>
+        <Heading textAlign={'center'} mb={5}>
+          Kontribusi
+        </Heading>
+        <Text textAlign={'center'} fontSize={'lg'} mb={5}>
+          Ingin <b>ikut berkontribusi?</b> pada pengembangan aplikasi ini. Silahkan kunjungi github repository berikut, untuk mendapatkan akses kode aplikasi ini
+        </Text>
+        <Link target={'_blank'} to={'https://github.com/radityohanif/sibi-sign-language-detection'}>
+          <Button colorScheme={'gray'}>
+            <Icon as={FiGithub} fontSize={'xl'} mr={2} />
+            Github
+          </Button>
+        </Link>
+      </Stack>
     </MobileLayout>
   )
 }
